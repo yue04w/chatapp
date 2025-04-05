2b2b3f29e737a6de063f8e0d61fa2611abdc5e07
Yue <113717283+yue04w@users.noreply.github.com>
2025-04-04 22:41:16 -0400

    Delete PremYue-Project/chatapp/user directory

--- PremYue-Project/chatapp/user/PremYue-Project/chatapp/chat/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('chat/<str:room_name>/', views.chat_room, name='chat_room')
-]
--- PremYue-Project/chatapp/user/PremYue-Project/chatapp/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from users.views import login_page, logout_page, register_view
-from . import views
-
-urlpatterns = [
-    path('', login_page, name="login"),
-    path('logout/', logout_page, name="logout"),
-    path('register/', register_view, name="register"),
-]
